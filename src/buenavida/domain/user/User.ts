cf04f0453a73d8dc8ba899a060c69a617a977c2d
Alejandro <jean.pena,2022@upb.edu.co>
2025-03-10 11:29:59 -0500

    sync

--- src/buenavida/domain/user/User.ts
+++
@@ -1,9 +0,0 @@
-import AbstractUser, { UserInterface } from "./AbstractUser";
-
-export default class User extends AbstractUser {
-    constructor(userInterface: UserInterface) {
-        super(userInterface);
-    }
-    public isNull = (): boolean => false;
-
-}
